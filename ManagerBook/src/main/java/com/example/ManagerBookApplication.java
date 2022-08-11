31eee5a582e7cc04649c2c90005d66ac4dc11c55
Vancong01 <tavancong1994@gmail.com>
2022-08-11 10:28:51 +0700

    first commit

---
+++ ManagerBook/src/main/java/com/example/ManagerBookApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagerBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManagerBookApplication.class, args);
+    }
+
+}
